cd96411e66e9ae57d0f2f764d286fda4833a5bc3
Joélia Rocha <80907796+joelia-rocha@users.noreply.github.com>
2026-06-24 19:54:45 -0300

    Add toggle function for menu visibility

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+function toggle() {
+    var sec = document.getElementById('home');
+    var nav = document.getElementById('navigation');
+    
+    // Liga e desliga a classe active para abrir/fechar o menu
+    sec.classList.toggle('active');
+    nav.classList.toggle('active');
+}
 
